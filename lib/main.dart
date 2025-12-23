c31dcb085c3f73a1a37211f2d0f63a0eff531a17
Firza Aurellia Iskandar <firzaiskandar6151@gmail.com>
2025-12-23 10:12:12 +0700

    last commit bismillah

--- lib/main.dart
+++ lib/main.dart
@@ -4,6 +4,7 @@ import 'package:vokapedia/screen/splash_screen.dart';
 import 'firebase_options.dart';
 
 void main() async {
+  
   WidgetsFlutterBinding.ensureInitialized();
 
   await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
